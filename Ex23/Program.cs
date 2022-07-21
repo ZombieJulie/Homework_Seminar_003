d7abf1c55902199eb326869dac8ff8ef9e3b4b7f
Салопаев Алексей <hegin4@gmail.com>
2022-07-21 16:43:52 +1000

    Косметические правка

--- Ex23/Program.cs
+++ Ex23/Program.cs
@@ -1,15 +1,12 @@
 ﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
 Console.WriteLine("Введите число: ");
-int n = int.Parse(Console.ReadLine());
+var n = int.Parse(Console.ReadLine());
 
 void getCube(int n)
 {
-Console.WriteLine($"Таблица кубов для числа: {n}");
-for (int i = 1; i <= n; i++){
-Console.WriteLine(Math.Pow(i,3));
-}
-
+    Console.WriteLine($"Таблица кубов для числа: {n}");
+    for (var i = 1; i <= n; i++) Console.WriteLine(Math.Pow(i, 3));
 }
 
 getCube(n);
